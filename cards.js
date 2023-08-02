2160c567905f8334dcf178156c526a945e8a2ade
Nicolo Rossi <redsnic@Nicolos-MacBook-Pro.fritz.box>
2023-08-02 11:42:41 +0200

    continuos updates

--- cards.js
+++ cards.js
@@ -7,11 +7,10 @@ arr = [
         "title" : "Urotori",
         "url" : "https://raw.githubusercontent.com/FluffyTailRedDoggo/Art/main/Nick/urotori%401%20%40urotori/Nick%20chibi.png" 
     }
-]
+];
 
-cardInit = function(){
-    $.each(arr, function(i) {
-        var templateString = '<div class="card mb-2 box-shadow">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
-        $('#card-container').append(templateString);
-    })
-}
+
+$.each(arr, function(i) {
+    var templateString = '<div class="card mb-2 box-shadow">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
+    $('#card-container').append(templateString);
+})
