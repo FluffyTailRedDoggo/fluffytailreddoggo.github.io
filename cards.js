f54d649723d30c5e8a027731a4b3dec317aafa7a
Nicolo Rossi <redsnic@Nicolos-MacBook-Pro.fritz.box>
2023-08-02 11:12:07 +0200

    continuos updates

---
+++ cards.js
@@ -0,0 +1,15 @@
+arr = [
+    {
+        "title" : "My things",
+        "url" : "https://raw.githubusercontent.com/FluffyTailRedDoggo/Art/main/My%20_art_works/nick.jpg" 
+    },
+    {
+        "title" : "Urotori",
+        "url" : "https://raw.githubusercontent.com/FluffyTailRedDoggo/Art/main/Nick/urotori%401%20%40urotori/Nick%20chibi.png" 
+    }
+]
+
+$.each(arr, function(i) {
+    var templateString = '<div class="card mb-2 box-shadow">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
+    $('#test').append(templateString);
+  })
