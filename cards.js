352faec2014639a58d627ad4fd28c4652d3e7c7a
Nicolo Rossi <redsnic@Nicolos-MBP.fritz.box>
2023-08-02 15:03:56 +0200

    continuos updates

--- cards.js
+++ cards.js
@@ -10,7 +10,7 @@ arr = [
 ];
 
 
-$.each(arr, function(i) {
-    var templateString = '<div class="card text-white bg-dark mb-3 m-3" style="width: 18rem;">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
-    $('#card-container').append(templateString);
-})
+//$.each(arr, function(i) {
+//    var templateString = '<div class="card text-white bg-dark mb-3 m-3" style="width: 18rem;">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
+//    $('#card-container').append(templateString);
+//})
--- search.js
+++ search.js
@@ -1,33 +1,36 @@
-const dirTree = require("directory-tree");
-const tree = dirTree("res/", ['.jpg', '.png']);
-
-console.log(tree)
-
-var default_image = "https://raw.githubusercontent.com/FluffyTailRedDoggo/fluffytailreddoggo.github.io/icons/folder.png"
-
-make_array = function(prefix, basepath, localpath){
-    var tree = dirTree(basepath + localpath, ['.jpg', '.png', '']);
-    var output = []
-    tree.children.forEach(function(element){
-        if(element.name.endsWith(".jpg") || element.name.endsWith(".png")){
-            output.append({
-                "title" : element.name,
-                "url" : prefix + basepath + localpath + element.name
-            })
-        }else{
-            output.append({
-                "title" : element.name,
-                "url" : default_image
-            })
-        }
+require(['require', 'directory-tree'], function(require){
+    const dirTree = require("directory-tree");
+    const tree = dirTree("res/", ['.jpg', '.png']);
+    
+    console.log(tree)
+    
+    var default_image = "https://raw.githubusercontent.com/FluffyTailRedDoggo/fluffytailreddoggo.github.io/icons/folder.png"
+    
+    make_array = function(prefix, basepath, localpath){
+        var tree = dirTree(basepath + localpath, ['.jpg', '.png', '']);
+        var output = []
+        tree.children.forEach(function(element){
+            if(element.name.endsWith(".jpg") || element.name.endsWith(".png")){
+                output.append({
+                    "title" : element.name,
+                    "url" : prefix + basepath + localpath + element.name
+                })
+            }else{
+                output.append({
+                    "title" : element.name,
+                    "url" : default_image
+                })
+            }
+        })
+        return output
+    }
+    
+    var default_basepath = "https://raw.githubusercontent.com/FluffyTailRedDoggo/fluffytailreddoggo.github.io/"
+    var res_folder = 'res/'
+    
+    $.each(make_array(default_basepath, res_folder, ""), function(i) {
+        var templateString = '<div class="card text-white bg-dark mb-3 m-3" style="width: 18rem;">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
+        $('#card-container').append(templateString);
     })
-    return output
-}
-
-var default_basepath = "https://raw.githubusercontent.com/FluffyTailRedDoggo/fluffytailreddoggo.github.io/"
-var res_folder = 'res/'
-
-$.each(make_array(default_basepath, res_folder, ""), function(i) {
-    var templateString = '<div class="card text-white bg-dark mb-3 m-3" style="width: 18rem;">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
-    $('#card-container').append(templateString);
 })
+
