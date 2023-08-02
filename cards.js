67f860d49f8417142da66c19c8afe6630c3f3b01
Nicolo Rossi <redsnic@Nicolos-MacBook-Pro.fritz.box>
2023-08-02 11:32:50 +0200

    continuos updates

--- cards.js
+++ cards.js
@@ -9,7 +9,9 @@ arr = [
     }
 ]
 
-$.each(arr, function(i) {
-    var templateString = '<div class="card mb-2 box-shadow">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
-    $('#card-container').append(templateString);
-  })
+cardInit = function(){
+    $.each(arr, function(i) {
+        var templateString = '<div class="card mb-2 box-shadow">    <img class="card-img-top" src="'+ arr[i].url +'" alt="Card image cap">      <div class="card-body">  <h5>' + arr[i].title + '</h5> </p></div> </div>';
+        $('#card-container').append(templateString);
+    })
+}
