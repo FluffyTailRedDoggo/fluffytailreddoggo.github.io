aee97f88eab888aa00e6e47229043a64726d8987
Nicolo Rossi <redsnic@Nicolos-MBP.fritz.box>
2023-08-02 13:40:29 +0200

    continuos updates

--- crawler.js
+++ crawler.js
@@ -1,34 +1,36 @@
-require(['require', 'crawler'])
-const Crawler = require('crawler'); 
+require(['crawler'], function(crawler){
+    const Crawler = require('crawler'); 
 
-const c = new Crawler({
-    maxConnections: 10,
-    // This will be called for each crawled page
-    callback: (error, res, done) => {
-        if (error) {
-            console.log(error);
-        } else {
-            const $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($('title').text());
+    const c = new Crawler({
+        maxConnections: 10,
+        // This will be called for each crawled page
+        callback: (error, res, done) => {
+            if (error) {
+                console.log(error);
+            } else {
+                const $ = res.$;
+                // $ is Cheerio by default
+                //a lean implementation of core jQuery designed specifically for the server
+                console.log($('title').text());
+            }
+            done();
         }
-        done();
-    }
-});
-
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-    uri: 'https://raw.githubusercontent.com/FluffyTailRedDoggo/Art/main/',
-    jQuery: false,
-
-    // The global callback won't be called
-    callback: (error, res, done) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Grabbed', res.body.length, 'bytes');
+    });
+    
+    // Queue URLs with custom callbacks & parameters
+    c.queue([{
+        uri: 'https://raw.githubusercontent.com/FluffyTailRedDoggo/Art/main/',
+        jQuery: false,
+    
+        // The global callback won't be called
+        callback: (error, res, done) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Grabbed', res.body.length, 'bytes');
+            }
+            done();
         }
-        done();
-    }
-}]);
+    }]);
+})
+
