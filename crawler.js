0ff06d55038e64ef6e8a3a143167138d53c81777
Nicolo Rossi <redsnic@Nicolos-MBP.fritz.box>
2023-08-02 13:04:08 +0200

    continuos updates

---
+++ crawler.js
@@ -0,0 +1,33 @@
+const Crawler = require('crawler');
+
+const c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: (error, res, done) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($('title').text());
+        }
+        done();
+    }
+});
+
+// Queue URLs with custom callbacks & parameters
+c.queue([{
+    uri: 'http://parishackers.org/',
+    jQuery: false,
+
+    // The global callback won't be called
+    callback: (error, res, done) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Grabbed', res.body.length, 'bytes');
+        }
+        done();
+    }
+}]);
